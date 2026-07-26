1ab9587206e137644c6fe0d825614ce435c4a761
Alan Lopez <alandlopezg@outlook.com>
2026-07-25 22:47:05 -0300

    agregar configuraciones de temas

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 5).theme(),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Material App Bar'),
         ),
         body: Center(
           child: FilledButton.tonal(
