9edb2cfb51fb3cd5f4ca6629949c1d1ca5fc8d22
Alan Lopez <alandlopezg@outlook.com>
2026-07-27 03:53:33 -0300

    Design of bubble messages from her

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+
+        const SizedBox(height: 5),
+
+        _ImageBubble(),
+
+        const SizedBox(height: 10),
+        // ToDo: Imagn
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/30-d37eee83c3c2180de4edb7da6fa9f5b7.gif',
+        width: size.width,
+        height: 150,
+        fit: BoxFit.cover
+      ),
+    );
+  }
+}
