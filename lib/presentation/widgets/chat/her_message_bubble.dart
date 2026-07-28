7bdc55a422b32b9b218fdda1117ae8496fe4de38
Alan Lopez <alandlopezg@outlook.com>
2026-07-27 23:03:08 -0300

    finalizacion de diseños en la messageFieldBox

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -43,7 +43,16 @@ class _ImageBubble extends StatelessWidget {
         'https://yesno.wtf/assets/no/30-d37eee83c3c2180de4edb7da6fa9f5b7.gif',
         width: size.width,
         height: 150,
-        fit: BoxFit.cover
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric( horizontal: 10, vertical: 5),
+            child: Text('Mi Amor está enviando una imagen'),
+          );
+        },
       ),
     );
   }
